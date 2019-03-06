b4e24949922c4722b3736b9dd1f74733421737cf
masha-b <mashabudryte@gmail.com>
2019-03-06 17:28:46 +0000

    raising an error for empty docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,14 @@
-require 'bike'
+require './lib/bike.rb'
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    raise 'No bikes available' unless @bike
+    @bike
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,21 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
-  it "releases a Bike" do
-    expect(subject.release_bike.class).to eq Bike
+  # let's upgrade our syntax, to:
+  # use a '#' before a method name
+  # to imply that it is an instance
+  # method. Also look: nested describes!
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 end
