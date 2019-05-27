9c454c63698713d6b788ff99cc6682a07b639e3c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-05-27 20:43:15 +0100

    tidying folder

--- lib/bike.rb
+++
@@ -1,5 +0,0 @@
-class Bike
-  def working?
-    true
-  end
-end
--- spec/bike_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'bike'
-
-describe Bike do
- it {is_expected.to respond_to :working? }
- it "is working" do
-   expect(subject.working?).to eq true
- end
-end
