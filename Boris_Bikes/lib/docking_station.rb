e24600d73632f55dabbdb5a14073b453d3b934db
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-05-29 10:43:06 +0100

    refactor out the capcity to a default in code and in test

--- Boris_Bikes/lib/docking_station.rb
+++ Boris_Bikes/lib/docking_station.rb
@@ -1,4 +1,6 @@
 class DockingStation
+  DEFAULT_CAPACITY = 20
+  
   def initialize
     @bikes = []
   end
@@ -18,7 +20,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
--- Boris_Bikes/spec/docking_station_spec.rb
+++ Boris_Bikes/spec/docking_station_spec.rb
@@ -17,7 +17,7 @@ describe DockingStation do
     end
 
     it 'raises an error when full' do
-      20.times { subject.dock(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error('Docking Station full')
     end
   end
